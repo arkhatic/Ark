e53ea091d25554e07f7a12ad105bfc437cbc1d92
Arthur Sousa <speeedofc@gmail.com>
2023-04-08 12:38:21 -0300

    New visuals and content

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,24 @@
   <Sidebar />
   <!-- landing page -->
   <div className="absolute snap-proximity snap-y overflow-y-scroll overflow-x-hidden h-screen w-screen">
-  <div class="dark:bg-black bg-white h-screen snap-start" id="home">
-    <div class="flex flex-col justify-center h-full mx-12 text-center items-center lg:items-start xl:text-left lg:mx-24 snap-start">
-      <h1 class="text-7xl md:text-8xl lg:text-9xl  text-blue-600 font-display font-black">
-        Arkhatic, 
-      </h1>
+  <div class="dark:bg-black bg-white h-screen snap-start video-wrapper" id="home">
+    <video playsinline autoplay muted loop>
+      <source src="@/assets/background-video.mp4" type="video/mp4">
+    </video>
 
-      <h1 class="text-5xl md:text-6xl dark:text-white text-black font-extrabold">
-        award-winning <br>
-        composer and scorer.
-      </h1>
-
-      <a href="https://linktree.com/arkhatic" class="mt-4 text-2xl text-blue-600 font-extrabold">@arkhatic</a>
+    <div class="bg-filter h-screen w-screen">
+      <div class="flex flex-col justify-center h-full mx-12 text-center items-center lg:items-start xl:text-left lg:mx-24 header">
+        <h1 class="text-7xl md:text-8xl lg:text-9xl  text-teal-600 font-display font-black">
+          Arkhatic, 
+        </h1>
+  
+        <h1 class="text-5xl md:text-6xl dark:text-white text-black font-extrabold">
+          award-winning <br>
+          composer and scorer.
+        </h1>
+  
+        <a href="https://linktree.com/arkhatic" class="mt-4 text-2xl text-teal-700 font-extrabold">@arkhatic</a>
+      </div>
     </div>
   </div>
 
@@ -23,23 +29,34 @@
   </div> 
 
   <!-- about page -->
-  <div class="bg-neutral-200 h-screen lg:h-auto p-12 lg:p-24 snap-start" id="about">
+  <div class="bg-neutral-200 h-auto p-12 lg:p-24 w-screen" id="about">
     <div class="flex flex-col text-left">
-      <h1 class="text-6xl md:text-7xl xl:text-8xl text-black font-extrabold font-display">
+      <h1 class="text-6xl md:text-7xl text-black font-extrabold font-display">
         I have always been learning alone.
       </h1>
       
       <div class="flex flex-row mt-4">
-        <p class="text-2xl lg:text-3xl text-black font-bold mt-4">
-          Nothing is easy. <br> <br>
-          
-          Years ago, I decided to study music, without having a tip of how difficult it really is. Without any support from my parents, I kept pressing on even when the times hardened quite a lot. <br> <br>
+        <p class="text-2xl text-black font-bold mt-4 max-w-full">
+          I am composer with with more than 5 years of experience composing screamingly loud and emotive music for games, films, and other media. I have worked on:
+          <br>
+          <br>
+          <ul class="lg:ml-12 list-disc">
+            <li>Draft in the woods;
+              <iframe src="https://store.steampowered.com/widget/2327730/" frameborder="0" width="100%" height="190" class="mt-4 lg:w-[600px]"></iframe>
+            </li>
 
-          When I got my first job as a composer, I felt, for the first time, <br> 
-          <span class="text-neutral-500">a glance of happiness</span>.<br>
+            <li>Reaper of Gods;</li>
 
-          So that is me, a ship who doesn't stop sailing, even when the sea is rough. <br> <br>
-          In other words, an <span class="text-neutral-500">ark</span>.
+            <li>Raifuejji.
+            </li>
+          </ul>
+
+          <br>
+          <br>
+
+          I have also worked on a few other projects, but I can't talk about them yet. 😅 <br>
+          Apart from orchestral music, I have a background with rock/metal, electronic, and ambient music. I am also a sound designer and a sound engineer.
+
         </p>
       </div>
     </div>  
@@ -50,8 +67,8 @@
     <div>
       <h1 class="text-5xl lg:text-6xl xl:text-7xl text-green-700 font-black font-display">Fortunately, I don't work alone anymore.</h1>
 
-      <h2 class="text-3xl lg:text-4xl text-black font-extrabold mt-8 lg:mt-4">
-        A <span class="text-green-600 text-4xl lg:text-5xl">huge</span> thanks to the guys below for helping me get into places I did not know were possible to reach and to overcome the challenges of every day. ❤️
+      <h2 class="text-3xl text-black font-extrabold mt-8 lg:mt-4">
+        A <span class="text-green-600 text-4xl">huge</span> thanks to the guys below for helping me get into places I did not know were possible to reach and to overcome the challenges of every day. ❤️
       </h2>
     </div>
 
@@ -63,7 +80,7 @@
             src="https://cdn.discordapp.com/attachments/853715361568129054/1033771155590484088/PESADELUM_2.webp" />
         </a>
 
-        <a href="https://wonderacid-studio.vercel.app">
+        <a href="https://wonderacid.web.app">
           <img 
             class="w-full filter grayscale hover:grayscale-0 rounded-xl"
             src="https://cdn.discordapp.com/attachments/853715361568129054/1033771276206100581/logo-squared.png" />
@@ -80,7 +97,7 @@
 
   <!-- divisory -->
   <div class="bg-blue-800 h-auto p-24 text-center">
-    <h1 class="text-5xl lg:text-7xl text-white font-extrabold">Let us look at some of <span class="italic">my</span> projects.</h1>
+    <h1 class="text-5xl text-white font-extrabold">Let us look at some of <span class="italic">my</span> projects.</h1>
   </div> 
 
   <!-- the undead commander -->
@@ -147,9 +164,21 @@
     </p>
   </div>
 
+  <div class="bg-black h-screen flex flex-col justify-center items-center text-center snap-start" id="solo">
+    <img 
+      src="https://images6.alphacoders.com/843/thumb-1920-843864.jpg" 
+      class="w-full h-full object-cover opacity-50 absolute z-10 ">
+
+    <h1 class="text-white text-8xl font-extrabold z-0">Solo of the Sea</h1>
+
+    <div class="z-40 w-screen mt-2">
+      <MusicPlayer source="https://cdn.discordapp.com/attachments/763415124509196290/1094260190293016698/Solo_of_the_Sea.mp3" />
+    </div>
+  </div> 
+
   <!-- divisory -->
-  <div class="bg-orange-700 h-auto p-24 text-center">
-    <h1 class="text-5xl lg:text-6xl xl:text-7xl text-white font-extrabold">Soundscapes ahead!</h1>
+  <div class="bg-blue-700 h-auto p-24 text-center">
+    <h1 class="text-5xl text-white font-extrabold">Soundscapes</h1>
   </div> 
 
   <!-- soundscapes -->
@@ -176,7 +205,7 @@
       <h1 class="text-white text-8xl font-extrabold z-0">Battle</h1>
 
       <div class="z-40 w-full mt-2">
-        <MusicPlayer source="https://cdn.discordapp.com/attachments/853715361568129054/1041443658949472346/Arise.wav" />
+        <MusicPlayer source="https://cdn.discordapp.com/attachments/1091778371928727664/1091779703024660531/Gods.mp3" />
       </div>
     </div>
   </div>
@@ -209,20 +238,47 @@
     </div>
   </div>
 
+  <div class="h-[200vh] xl:flex xl:h-screen xl:w-screen xl:flex-row snap-start">
+    <div class="bg-black w-screen xl:w-[50vw] h-screen flex flex-col justify-center items-center text-center">
+      <img 
+        src="https://rare-gallery.com/uploads/posts/965957-temple-fantasy-art-artwork-landscape-mountains.jpg" 
+        class="w-screen xl:w-[50vw] h-screen object-cover opacity-50 absolute z-10 ">
+
+      <h1 class="text-white text-8xl font-extrabold z-0">Temple</h1>
+
+      <div class="z-40 w-full mt-2">
+        <MusicPlayer source="https://cdn.discordapp.com/attachments/1091778371928727664/1091779704463302757/Insolidus.mp3" />
+      </div>
+    </div>
+
+    <!-- wise fields -->
+    <div class="bg-black w-screen xl:w-[50vw] h-screen flex flex-col justify-center items-center text-center snap-start xl:snap-none">
+      <img 
+        src="https://i.pinimg.com/originals/1b/03/e0/1b03e03c6848859eeb7d18756488abce.jpg" 
+        class="w-screen xl:w-[50vw] h-screen object-cover opacity-50 absolute z-10 ">
+
+      <h1 class="text-white text-8xl font-extrabold z-0">Warfare</h1>
+
+      <div class="z-40 w-full mt-2">
+        <MusicPlayer source="https://cdn.discordapp.com/attachments/1091778371928727664/1091779702626205896/The_Deadlift.mp3" />
+      </div>
+    </div>
+  </div>
+
   <!-- contact -->
-  <div class="min-h-screen bg-blue-50 px-12 py-24 lg:px-24 flex flex-col text-center justify-center align-center snap-start" id="contact">
-    <h1 class="text-blue-700 font-black text-6xl lg:text-7xl xl:text-8xl m-0 p-0 ">
+  <div class="min-h-screen bg-blue-50 px-12 py-24 lg:px-24 flex flex-col text-center justify-center align-center" id="contact">
+    <h1 class="text-blue-700 font-black text-6xl m-0 p-0 ">
       That was a ride, wasn't it?
     </h1>
 
-    <p class="text-black font-bold text-3xl lg:text-4xl mt-4">
+    <p class="text-black font-bold text-3xl mt-4">
       I hope you enjoyed as much as I did making this website. <br>
       If you want to contact me, send me a message on Discord or an email. <br>
       I am always open to new projects and collaborations. <br>
       <span class="text-blue-600 font-black">I hope your light shines!</span> <br>
     </p>
 
-    <div class="mt-24 mx-auto w-full lg:w-4/6 2xl:w-1/2 flex-grow flex flex-col lg:flex-row items-center justify-between">
+    <div class="mt-24 mx-auto w-full flex-grow flex flex-col lg:flex-row items-center justify-center">
       <a href="https://discord.gg/km5rfvBdcZ" target="_blank" class="">
         <button 
           class="h-64 w-64 lg:h-72 lg:w-72 bg-white rounded-[2.5rem] flex flex-col items-center justify-center text-center text-xl font-bold text-[#5865F2] shadow-2xl hover:shadow-sm">
@@ -231,15 +287,6 @@
         </button>
       </a>
 
-      <div class="w-96 h-2 my-12 lg:w-2 lg:h-96 lg:my-0 lg:mx-12 bg-[#5865F2]"></div>
-
-      <a href="mailto:arkhatic.sousa@gmail.com" target="_blank" class="">
-        <button 
-          class="h-64 w-64 lg:h-72 lg:w-72 bg-white rounded-[2.5rem] flex flex-col items-center justify-center text-center text-xl font-bold text-[#5865F2] shadow-2xl hover:shadow-sm">
-          <img src="https://cdn.discordapp.com/attachments/853715361568129054/1033805220553175050/free-mail-icon-142-thumb.png" class="w-24 h-24 m-0" />
-          Send me an email!
-        </button>
-      </a>
     </div>
   </div>
 </div>
@@ -256,3 +303,33 @@ export default {
 }
 </script>
 
+<style scoped>
+video {
+  object-fit: cover;
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+}
+
+.video-wrapper {
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  overflow: hidden;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.header {
+  position: relative;
+  backdrop-filter: opacity(0.5);
+}
+
+.bg-filter {
+  backdrop-filter: blur(10px);
+}
+</style>
